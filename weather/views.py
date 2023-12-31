3201aaff19d637eaa383a24146462112856d4cda
shuvo-art <shuvo83qn@gmail.com>
2023-12-31 17:13:59 +0600

    Completed Weather Detector app using openweather api

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+import json 
+import urllib.request
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=1d36935f6788f23bfe3c2c0ad83d90fc').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon'])+' '+
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', { 'city': city, 'data': data})
